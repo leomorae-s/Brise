eb0ba57642dd7e9c1036d97ae755364f099fd168
morae-s <dev.leonardomoraes@gmail.com>
2026-07-04 02:36:30 -0300

    Added detekt to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
+    id("dev.detekt") version "2.0.0-alpha.5" apply false
 
 }
 
